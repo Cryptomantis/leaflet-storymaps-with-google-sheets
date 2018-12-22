cabe112536f7eaf1722fb050de8c1e093118c54f
Cryptomantis <35435458+Cryptomantis@users.noreply.github.com>
2018-12-21 20:33:09 -0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1gdTrLiaYC70zN5RnD_JWZtpzrOTdiN4ffj_E_VOl9yk/edit#gid=0/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1gdTrLiaYC70zN5RnD_JWZtpzrOTdiN4ffj_E_VOl9yk/';
